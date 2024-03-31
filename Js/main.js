2558cee9e565a7b4fd767fe7edf8908293b716e1
Clansou <clement.souplet78@gmail.com>
2024-03-31 12:03:04 +0200

    Update 0

--- Js/main.js
+++ Js/main.js
@@ -5,6 +5,6 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 getDate();
-DrawSix(ctx, canvas.width, canvas.height);
+DrawZero(ctx, canvas.width, canvas.height);
 
 // DrawFour, DrawFive, DrawSix, DrawSeven, DrawEight, DrawNine
--- Js/numbers/numbers.js
+++ Js/numbers/numbers.js
@@ -19,29 +19,27 @@ function fillColor(ctx){
 }
 
 export function DrawZero(ctx, canvasWidth, canvasHeight){
+    const { xOffset, yOffset, width, height } = calculateCoordinates(canvasWidth, canvasHeight);
 
-    const xOffset = canvasWidth * 0.3; // Décalage horizontal
-    const yOffset = canvasHeight * 0.2; // Décalage vertical
-    const width = canvasWidth * 0.4; // Largeur de l'ellipse
-    const height = canvasHeight * 0.6; // Hauteur de l'ellipse
-    const centerX = xOffset + width / 2; // Coordonnée x du centre de l'ellipse
-    const centerY = yOffset + height / 2; // Coordonnée y du centre de l'ellipse
-    const radiusX = width / 2; // Demi-largeur de l'ellipse
-    const radiusY = height / 2; // Demi-hauteur de l'ellipse
-    const centralRadiusX = radiusX * 0.6; // Demi-largeur du cercle central
-    const centralRadiusY = radiusY * 0.6; // Demi-hauteur du cercle central
 
-    // Dessiner l'ellipse avec la couleur de remplissage
     ctx.beginPath();
-    ctx.fillStyle = color;
-    ctx.ellipse(centerX, centerY, radiusX, radiusY, 0, 0, Math.PI * 2);
-    ctx.fill();
+    ctx.moveTo(xOffset + width * 0.1, yOffset);
+    ctx.lineTo(xOffset + width * 0.9, yOffset); 
+    ctx.lineTo(xOffset + width * 0.9, yOffset + height);
+    ctx.lineTo(xOffset + width * 0.1, yOffset + height);
+    ctx.lineTo(xOffset + width * 0.1, yOffset);
+    fillColor(ctx);
 
-    // Dessiner un cercle plus petit au centre pour masquer la partie centrale
     ctx.beginPath();
-    ctx.fillStyle = backgroundColor; // Couleur du fond, pourrait être transparente (rgba(0,0,0,0))
-    ctx.ellipse(centerX, centerY, centralRadiusX, centralRadiusY, 0, 0, Math.PI * 2);
-    ctx.fill();
+    ctx.moveTo(xOffset +width*0.3, yOffset+height*0.2);
+    ctx.lineTo(xOffset +width*0.7, yOffset+height*0.2);
+    ctx.lineTo(xOffset +width*0.7, yOffset+height*0.8);
+    ctx.lineTo(xOffset +width*0.3, yOffset+height*0.8);
+    ctx.lineTo(xOffset +width*0.3, yOffset+height*0.2);
+    ctx.stroke();
+    ctx.fillStyle = backgroundColor;
+    ctx.fill()
+
 }
 
 
